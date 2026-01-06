8f8fdd5c41c1ef80ef313fb34f83339c901cebe7
Martin Filla <freebsd@sysctl.cz>
2026-01-06 22:58:48 +0100

    bananapi-r64: support for mediatek mt7622-tphy
    Signed-off-by: Martin Filla <freebsd@sysctl.cz>

--- sys/arm64/mediatek/mt_xhci.c
+++ sys/arm64/mediatek/mt_xhci.c
@@ -266,20 +266,23 @@ mt_xhci_attach(device_t dev)
     }
 
     /* Enable PHYs */
-    for (i = 0; sc->soc->phy_names[i] != NULL; i++) {
-        if (sc->soc->phy_names[i] == NULL) {
-            continue;
-        }
+    for (i = 0; nitems(sc->phys); i++) {
         if (i >= nitems(sc->phys)) {
             device_printf(sc->dev,
                           "Too many phys present in DT.\n");
             return (EOVERFLOW);
         }
-        rv = phy_get_by_ofw_idx(sc->dev, node, i, &sc->phys[i]);
-        if (rv != 0)
-            break;   /* žádná další PHY */
+        if (sc->phys[i] == NULL) {
+            continue;
+        }
 
-        device_printf(sc->dev, "Found PHY index %d\n", i);
+        rv = phy_get_by_ofw_idx(sc->dev, node, i, &sc->phys[i]);
+        if (rv != 0) {
+            break;
+        }
+
+        device_printf(sc->dev, "This is '%s' phy\n",
+                      sc->soc->phy_names[i]);
     }
 
     for (i = 0; i < nitems(sc->phys); i++) {
